7f62dbe670a999ae7f158aafe838003e7aa53f3d
Brian Austin <austin.brian@gmail.com>
2017-05-31 14:59:28 -0400

    changes

--- blog_archive/code/austin_family.py
+++
@@ -1,8 +0,0 @@
-with open('../datasets/Austin_family_tree.txt','rU') as f:
-    data = [row.split('\n') for row in f]
-
-# First generation are the first two lines
-gen_1 = data[:1]
-
-# After the first generation, the gens are bracketed with 4 dashes
-for i in data[2:]
--- blog_archive/posts/times_gender_verbs.md
+++
@@ -1,2 +0,0 @@
-
-Take wsj story and apply it to the NY times, or another corpus of text looking for verbs and the ways that people describe things
